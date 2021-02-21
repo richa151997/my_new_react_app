aef7bfd35628e2d7cd915cfc0fffdfd4c3729c66
richa151997 <richa151997@gmail.com>
2021-02-21 20:10:16 +0530

    changed contact data

--- src/data/contactData.js
+++ src/data/contactData.js
@@ -1,29 +1,29 @@
 const contactData = [
     {
         id:1,
-        name: "Felix",
+        // name: "Felix",
         imgUrl: "http://placekitten.com/200/100",
         phone: "(212) 555-4567",
-        // email: "thecat@hotmail.com"
+        email: "thecat@hotmail.com"
     },
     {
         id:2,
         name: "Destroyer",
         imgUrl: "http://placekitten.com/400/300",
-        // phone: "(212) 555-345V6",
-        email: "ofworlds@yahoo.com"
+        phone: "(212) 555-345V6",
+        // email: "ofworlds@yahoo.com"
     },
     {
         id:3,
         name: "Fluffykins",
-        // imgUrl: "http://placekitten.com/400/200",
-        phone: "(212) 555-2345",
+        imgUrl: "http://placekitten.com/400/200",
+        // phone: "(212) 555-2345",
         email: "fluff@me.com"
     },
     {
         id:4,
-        // name: "Mr. Whiskerson",
-        imgUrl: "http://placekitten.com/300/200",
+        name: "Mr. Whiskerson",
+        // imgUrl: "http://placekitten.com/300/200",
         phone: "(212) 555-1234",
         email: "mr.whiskaz@catnap.meow"
     }
